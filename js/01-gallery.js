3df55b9d33fcd8f84df97afa4a3e1925d5ffbcdc
Olia Medvetska <olia.medvetska1967@gmail.com>
2023-03-25 21:51:56 +0200

    add hw-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector('.gallery');
+const galleryEl = createGalery(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryEl);
+
+function createGalery() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li> `;
+    })
+    .join('');
+}
+
+const onGalleryContainerClick = event => {
+  event.preventDefault();
+
+  const source = event.target.dataset.source;
+  if (!source) return;
+
+  const instance = basicLightbox.create(`
+    <img src="${source}" width="800" height="600">`);
+
+  instance.show();
+
+  document.addEventListener(
+    'keydown',
+    event => {
+      if (event.code === 'Escape') {
+        instance.close();
+      }
+    },
+    { once: true }
+  );
+};
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
 console.log(galleryItems);
